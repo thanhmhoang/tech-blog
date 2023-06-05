70afb18c8bc4a89b0c10dc886fc64a782f7dc54b
ThanhHoang15 <thanh.hoang210@gmail.com>
2023-06-05 09:07:53 -0700

    fixed bugs

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require('User');
-const Post = require('Post');
-const Comment = require('Comment');
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
 Post.belongsTo(User, {
   foreignKey: 'userId',
