d8bfc701ddcdf69278be4a28a3faac6e67c427b9
ThanhHoang15 <thanh.hoang210@gmail.com>
2023-06-07 14:43:01 -0700

    add jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 //allow for use of .env data
 require('dotenv').config();
 
-// create connection to db, whether local on on heroku using jawsdb
+// create connection to db, whether local or on heroku using jawsdb
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PW, {
