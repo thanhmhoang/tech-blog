f8f55a606dafec44dd4e6824e646b20b50440e35
ThanhHoang15 <thanh.hoang210@gmail.com>
2023-06-07 08:15:38 -0700

    v5

--- models/index.js
+++ models/index.js
@@ -1,24 +1,20 @@
-const Comment = require('./Comment.js');
-const User = require('./User.js');
-const Post = require('./Post.js');
+const Comment = require("./Comment");
+const User = require("./User");
+const Post = require("./Post");
 
 Post.belongsTo(User, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE'
+  foreignKey: "userId",
+  onDelete: "CASCADE",
 });
 
 Post.hasMany(Comment, {
-  foreignKey: 'postId',
-  onDelete: 'CASCADE'
+  foreignKey: "postId",
+  onDelete: "CASCADE",
 });
 
 Comment.belongsTo(User, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE'
+  foreignKey: "userId",
+  onDelete: "CASCADE",
 });
 
-module.exports = {
-  Comment,
-  User,
-  Post
-};
+module.exports = { Comment, User, Post };
