2290279494e81011541f7a714e5cb1050293318e
ThanhHoang15 <thanh.hoang210@gmail.com>
2023-06-07 08:12:12 -0700

    v3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-
+const routes = require('./controllers')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require('./controllers/'));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
